015d4257cd35d35955e41cb07ece3b20e70f9875
Molkov Anton <molkov374@gmail.com>
2025-01-19 18:29:01 +0300

    Обработчик xlsx файлов
    .

---
+++ src/utilities/xlsxFIle.js
@@ -0,0 +1,31 @@
+const ExcelJS = require('exceljs');
+
+async function xlsxFileHandler(buffer) {
+    const workbook = new ExcelJS.Workbook();
+    try {
+        await workbook.xlsx.load(buffer);
+        const worksheet = workbook.worksheets[0]; 
+      
+        if(worksheet.rowCount === 0){
+            console.log('Файл пустой.')
+            return {success : false }; 
+        }
+
+        const data = [];
+
+        worksheet.eachRow((row, rowNumber) => {
+            if (rowNumber > 1) { 
+                const rowData = row.values.slice(1);  
+                data.push(rowData);
+            }
+        });
+
+        return { success: true, data};
+    } catch (err) {
+        return { success: false };
+    }
+}
+
+module.exports = {
+    xlsxFileHandler
+};
