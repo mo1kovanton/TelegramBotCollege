54e838f0aacb3adb4c81ab6043a92e495270a6c2
Molkov Anton <molkov374@gmail.com>
2025-01-23 23:17:57 +0300

    Задание 3 (без реализации)

---
+++ src/commands/lessonTopic.js
@@ -0,0 +1,5 @@
+module.exports = (bot) => {
+  bot.command('lesson_topic',async (ctx) => {
+    await ctx.reply(`К сожалению, команда для проверки корректности ввода темы занятия не была реализована.`);
+  });
+};;
